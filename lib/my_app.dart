b30399572717f06d016b3579b118e8f9b257d2f5
mnikley <matthias.ley@pm.me>
2023-04-23 21:30:15 +0200

    WIP geolocations

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(const HedgeProfilerApp());
 
@@ -122,7 +123,9 @@ Page resource error:
                   _showNameForm = false;
                 });
                 _loadPage(context,
-                    'https://maps.arcanum.com/en/map/europe-19century-secondsurvey/?bbox=703865.3388931998%2C6876591.638315973%2C1744020.4197978782%2C7349889.717457783&map-list=1&layers=osm%2C158%2C164');
+                    // 'https://maps.arcanum.com/en/map/europe-19century-secondsurvey/?bbox=703865.3388931998%2C6876591.638315973%2C1744020.4197978782%2C7349889.717457783&map-list=1&layers=osm%2C158%2C164'
+                    'https://maps.arcanum.com/'
+                );
                 Navigator.pop(context);
               },
             ),
@@ -134,7 +137,7 @@ Page resource error:
                   _showNameForm = false;
                 });
                 _loadPage(context,
-                    'https://bodenkarte.at/#/center/16.3191,48.1968/zoom/13');
+                    'https://bodenkarte.at/');
                 Navigator.pop(context);
               },
             ),
@@ -165,9 +168,41 @@ Page resource error:
   }
 
   void _loadPage(BuildContext context, String url) async {
+    // prevent loading of page if page was already loaded before
     String stem = RegExp("http.*(com|at)").firstMatch(url)?.group(0) ?? '';
     if (_currentUrlStem != stem && stem != '') {
       _currentUrlStem = stem;
+
+      // update geoinfo
+      _updateLocation();
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      var latitude = prefs.getString("latitude");
+      var longitude = prefs.getString("longitude");
+
+      // if no latitude and longitude is stored in memory variables, fetch last location
+      if (latitude == null || longitude == null) {
+        Position? pos = await _getLastKnownLocation();
+
+        // if no last location is known, use viennese coordinates
+        if (pos == null) {
+          latitude = "16.3389";
+          longitude = "48.1970";
+        } else {
+          latitude = pos.latitude.toString();
+          longitude = pos.longitude.toString();
+        }
+      }
+
+      // re-build boden karte URL with updated coords
+      if (url.startsWith("https://bodenkarte.at")) {
+        url = "https://bodenkarte.at/#/center/$latitude,$longitude/zoom/10";
+      }
+
+      if (url.startsWith("https://maps.arcanum.com/")) {
+        // TODO: figure out how bbox is calculated based on lat / lon
+
+      }
 
       // load page via controller
       _controller.loadRequest(Uri.parse(url));
@@ -197,12 +232,17 @@ class _NameFormState extends State<NameForm> {
   void initState() {
     super.initState();
     _populateInputFields();
+    _updateLocation();  // TODO: error handling, display geo information is unavailable somehow
+    // TODO: showAlertDialog() can be used, needs context
   }
 
   void _populateInputFields() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     _nameController.text = prefs.getString('name') ?? '';
     _numberController.text = prefs.getString('number') ?? '';
+
+    // get geo location
+    _updateLocation();
   }
 
   void _persistInputStorage() async {
@@ -217,9 +257,12 @@ class _NameFormState extends State<NameForm> {
     _populateInputFields();
   }
 
-  void _saveFormData() {
+  void _saveFormData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var position  = await _getLastKnownLocation();
     //TODO: implement writing to database
     final name = _nameController.text.trim();
+    showAlertDialog(context);
   }
 
   @override
@@ -288,4 +331,83 @@ class _NameFormState extends State<NameForm> {
       ),
     );
   }
+}
+
+/// Determine the current position of the device.
+///
+/// When the location services are not enabled or permissions
+/// are denied the `Future` will return an error.
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
+}
+
+Future<Position?> _getLastKnownLocation() async {
+  return await Geolocator.getLastKnownPosition(forceAndroidLocationManager: true);
+}
+
+void _updateLocation() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var position = await _determinePosition();
+  prefs.setString("latitude", position.latitude.toString());
+  prefs.setString("longitude", position.longitude.toString());
+}
+
+showAlertDialog(BuildContext context) {
+
+  // set up the button
+  Widget okButton = TextButton(
+    child: const Text("Close"),
+    onPressed: () { Navigator.of(context).pop(); },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: const Text("Geocoordinates not available"),
+    content: const Text("Could not get location. Make sure Location services are "
+        "enabled (Android: Settings -- >Location --> Enable)."),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
