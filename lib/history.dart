0711ef491da6c9d10491ffbf7bf9d61df5d1182c
Mnikley <mnikley89@gmail.com>
2024-03-23 15:03:00 +0100

    added dark-mode colors to history widget

--- lib/history.dart
+++ lib/history.dart
@@ -43,7 +43,11 @@ class _PdfFileListState extends State<PdfFileList> {
   Widget build(BuildContext context) {
     bool hasExistingPdfFiles = _files.isNotEmpty;
 
+    Color backgroundColor = widget.darkMode ? MyColors.sideBarBackground : MyColors.white;
+    Color textColor = widget.darkMode ? MyColors.white80 : MyColors.black;
+
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(
         title: Text(widget.currentLocale == "EN"
             ? "Hedge Rating History"
@@ -51,17 +55,17 @@ class _PdfFileListState extends State<PdfFileList> {
       ),
       body: Column(
         children: <Widget>[
-          const Divider(
+          Divider(
             height: 1,
-            color: MyColors.black,
+            color: textColor,
           ),
           const SizedBox(height: 10),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: hasExistingPdfFiles
-                  ? createListOfExistingHedgePdfs()
-                  : noHedgesYetText(),
+                  ? createListOfExistingHedgePdfs(textColor)
+                  : noHedgesYetText(textColor),
             ),
           ),
         ],
@@ -69,22 +73,22 @@ class _PdfFileListState extends State<PdfFileList> {
     );
   }
 
-  Widget noHedgesYetText() {
+  Widget noHedgesYetText(Color color) {
     return Text(
         widget.currentLocale == "EN"
             ? "No hedge reviews available yet. Get started by rating your first hedge and manage all your results here!"
             : "Bisher sind keine Heckenbewertungen vorhanden. Beginnen Sie mit der Bewertung Ihrer ersten Hecke und verwalten Sie hier alle Ihre Ergebnisse!",
-        style: const TextStyle(color: MyColors.black, fontSize: 16));
+        style: TextStyle(color: color, fontSize: 16));
   }
 
-  Widget createListOfExistingHedgePdfs() {
+  Widget createListOfExistingHedgePdfs(Color color) {
     return ListView.separated(
       itemCount: _files.length,
       itemBuilder: (context, index) {
         String fileName = _files[index].path.split("/").last;
 
         return ListTile(
-          title: sanitizeFileName(fileName),
+          title: sanitizeFileName(fileName, color),
           trailing: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
@@ -143,7 +147,7 @@ class _PdfFileListState extends State<PdfFileList> {
     );
   }
 
-  Text sanitizeFileName(String fileName) {
+  Text sanitizeFileName(String fileName, Color color) {
     try {
       List<String> dateStringArray = fileName
           .replaceFirst("hedge_profiler_", "")
@@ -151,9 +155,9 @@ class _PdfFileListState extends State<PdfFileList> {
           .split("_");
       String date = formatDate(dateStringArray[0]);
       String time = formatTime(dateStringArray[1]);
-      return Text("$date $time");
+      return Text("$date $time", style: TextStyle(color: color));
     } catch (e) {
-      return Text(fileName);
+      return Text(fileName, style: TextStyle(color: color));
     }
   }
 
